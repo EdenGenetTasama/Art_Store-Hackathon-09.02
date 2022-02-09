a3a1b36a1c5edd47228ef868205dc739334f852c
EdenGenetTasama <edentasama19@gmail.com>
2022-02-09 22:57:43 +0200

    add router

---
+++ src/Components/parts/Homepage/FirtstSection/firstSection.jsx
@@ -0,0 +1,10 @@
+
+const FirstSection=()=>{
+
+    return(<div>
+        
+
+    </div>)
+}
+
+export default FirstSection;
--- src/Components/parts/Navbar/navbar.jsx
+++ src/Components/parts/Navbar/navbar.jsx
@@ -1,8 +1,12 @@
+import {Link} from "react-router-dom";
+
+
 const Navbar=()=>{
-
-    return(<div>
-
-    </div>)
+    return(
+    <div>
+        <Link to="/"><button>Home</button></Link>
+    </div>
+)
 }
 
 export default Navbar;
