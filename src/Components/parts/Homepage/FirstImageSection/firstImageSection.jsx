91bfdf805a09bc68e79f816f9193cdfeb1727088
EdenGenetTasama <edentasama19@gmail.com>
2022-02-10 07:16:11 +0200

    add style

--- src/Components/parts/Homepage/FirstImageSection/firstImageSection.jsx
+++ src/Components/parts/Homepage/FirstImageSection/firstImageSection.jsx
@@ -3,7 +3,7 @@ import "./firstImageSection.css";
 const FirstImageSection=()=>{
 
     return(<div>
-        <h1>image</h1>
+        <h1 className="HeaderLine">Welcome ☺ </h1>
         <img src="https://cdn.pixabay.com/photo/2017/09/12/11/56/universe-2742113_960_720.jpg" alt="spaceImage" className="homeImage"/>
     </div>)
 }
