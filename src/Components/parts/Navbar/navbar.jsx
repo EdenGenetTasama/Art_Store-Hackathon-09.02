b8ec3ebbe22e85c2c9e6fe4fcc44b66ba1202fb1
EdenGenetTasama <edentasama19@gmail.com>
2022-02-10 04:16:09 +0200

    fix navbar desing

--- src/Components/parts/Navbar/navbar.jsx
+++ src/Components/parts/Navbar/navbar.jsx
@@ -1,31 +1,30 @@
 import { Link } from "react-router-dom";
 import "./navbar.css";
 
- const Navbar=()=>{
-
-  return(
-    <div class="navbar">
+const Navbar = () => {
+  return (
+      <div class="navbar">
         <ul class="nav">
           <li class="item">
-          <Link to="/">Home</Link>
+            <Link to="/">Home</Link>
           </li>
           <li class="item">
-          <Link to="/painting">Painting</Link>
-          </li>
-
-          <li class="item">
-          <Link to="/sculpture">Sculpture</Link>
+            <Link to="/painting">Painting</Link>
           </li>
 
           <li class="item">
-          <Link to="/photography">Photography</Link>
+            <Link to="/sculpture">Sculpture</Link>
+          </li>
+
+          <li class="item">
+            <Link to="/photography">Photography</Link>
           </li>
         </ul>
 
         <div class="logo">
-          <img src="#"/>
+          <i class="fa fa-users fa-2x" aria-hidden="true"></i>
         </div>
       </div>
-  )
- }
+  );
+};
 export default Navbar;
